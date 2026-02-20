c3aea3ad3abab0641763641e6e91214cc4f8fc2e
Hayden <136758886+haydenacarr@users.noreply.github.com>
2026-02-20 09:17:59 +0000

    added otsu from lecture

--- main.py
+++ main.py
@@ -12,10 +12,48 @@ def threshold(thresh, img):
                 img[x,y] = 0
     return img
 
+# Otsu method for thresholding (code adapted from lecture 2)
+def otsu(img):
+    hist = [0]*img.size
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            hist[img[i, j]] += 1
+
+    total_pixels = img.shape[0] * img.shape[1]
+
+    total_sum = 0
+    for t in range(256):
+        total_sum += t * hist[t]
+
+    wBackground = 0
+    wForeground = 0
+
+    sumB = 0
+    varMax = 0
+    thresh = 0
+
+    for t in range(256):
+        wBackground += hist[t]
+        if wBackground == 0: continue
+
+        wForeground = total_pixels - wBackground
+        if wForeground == 0: break
+
+        sumB += t * hist[t]
+        mBackground = sumB / wBackground
+        mForeground = (total_sum - sumB) / wForeground
+
+        varBetween = wBackground * wForeground * (mBackground - mForeground) * (mBackground - mForeground)
+        if varBetween > varMax:
+            varMax = varBetween
+            thresh = t
+
+    return thresh
+
 #read in an image into memory
 for i in range(1,16):
-    img = cv.imread('Orings/Oring' + str(i) + '.jpg')
-    thresh = 100
+    img = cv.imread('Orings/Oring' + str(i) + '.jpg', 0)
+    thresh = otsu(img)
     bw = threshold(thresh, img)
     rgb = cv.cvtColor(bw, cv.COLOR_GRAY2RGB)
     cv.imshow("Thresholded Image", rgb)
